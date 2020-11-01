b1783cf4d65539ebf513f0b063dba72c19f444f0
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-01 16:42:58 +0100

    App &  home component modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Router from './router/router';
 import './assets/css/style.css';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
--- src/views/home.js
+++ src/views/home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/header';
+import Main from '../components/main';
 
 function Home() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
