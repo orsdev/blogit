ad966ee605b5705bb6f206993a0a30dbe883f1ac
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-01 16:42:36 +0100

    main component added

---
+++ src/components/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Featured from './featured';
+
+function Main() {
+  return (
+    <main className="main">
+      <Featured />
+      <div className="main-body">Body</div>
+    </main>
+  );
+}
+
+export default Main;
