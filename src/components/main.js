345411168d72a1968b650f8414f7b65c433cb464
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-02 15:59:47 +0100

    Pagination component imported into main.js component

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BlogCards from './blogCards';
 import Featured from './featured';
+import Pagination from './pagination';
 
 function Main() {
   return (
     <main className="main mb-5">
       <Featured />
       <BlogCards />
+      <Pagination />
     </main>
   );
 }
