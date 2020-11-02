603d16a333e3e5108cf7513ab943b7ccbca42cdf
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-02 16:00:21 +0100

    login route defined in router.js

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Login from '../components/login';
 import Home from '../views/home';
 
 function Router() {
   return (
     <>
       <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
     </>
   );
 }
