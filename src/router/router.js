e6804c38f594a8da49e7d5d16ee03347a0287445
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-03 10:20:20 +0100

    404 component imported in router.js component

--- src/router/router.js
+++ src/router/router.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from '../components/login';
 import Signup from '../components/signup';
-import SinglePost from '../components/singlePost';
+import SinglePost from '../views/singlePost';
 import Home from '../views/home';
+import Page404 from '../views/404';
 
 function Router() {
   return (
     <>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/join" component={Signup} />
-      <Route exact path="/post/:title" component={SinglePost} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/join" component={Signup} />
+        <Route exact path="/post/:title" component={SinglePost} />
+        <Route component={Page404} />
+      </Switch>
     </>
   );
 }
