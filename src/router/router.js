feabcfcf14742a46f6911eba6a0b0089fb8fe76b
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-03 10:06:21 +0100

    singlePost component imported and used in router.js component

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from '../components/login';
 import Signup from '../components/signup';
+import SinglePost from '../components/singlePost';
 import Home from '../views/home';
 
 function Router() {
@@ -10,6 +11,7 @@ function Router() {
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/join" component={Signup} />
+      <Route exact path="/post/:title" component={SinglePost} />
     </>
   );
 }
