82613c34b3fff6a417833f599df70e7c51a3f211
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-08 16:06:13 +0100

    user types defined in type.js

--- src/redux/type.js
+++ src/redux/type.js
@@ -1,6 +1,8 @@
 const actionTypes = {
   LOGIN_USER: 'LOGIN_USER',
-  LOGOUT_USER: 'LOGOUT_USER'
+  LOGOUT_USER: 'LOGOUT_USER',
+  SET_USER: 'SET_USER',
+  REMOVE_USER: 'REMOVE_USER'
 };
 
 export default actionTypes;
