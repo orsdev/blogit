0bfd414eb9c685df47d62d69a1de188ca9f418cd
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-08 16:06:52 +0100

    userReducer added to combined in root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import loginReducer from './reducers/login.reducer';
+import userReducer from './reducers/user.reducer';
 
-const rootReducer = combineReducers({ login: loginReducer });
+const rootReducer = combineReducers({ login: loginReducer, user: userReducer });
 
 export default rootReducer;
