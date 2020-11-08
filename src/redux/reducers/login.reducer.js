3b4fdb40e76fcb9015a265559a0432c371fe0d4e
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-08 10:29:37 +0100

    login reducer added to app

---
+++ src/redux/reducers/login.reducer.js
@@ -0,0 +1,27 @@
+import actionTypes from '../type';
+
+const initialState = {
+  token: null,
+  error: null
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.LOGIN_USER:
+      return {
+        ...state,
+        token: action.payload.token,
+        error: action.payload.error
+      };
+    case actionTypes.LOGOUT_USER:
+      return {
+        ...state,
+        token: null,
+        error: null
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
