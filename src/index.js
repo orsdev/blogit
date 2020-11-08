7a24e48ceaf88d3f9785485e7a125259e21a51f2
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-08 10:38:10 +0100

    Redux Provider wrapped around App component in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import store from './redux/store';
 
 const app = (
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
