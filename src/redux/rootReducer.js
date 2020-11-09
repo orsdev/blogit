5de20dee621abf2dffa3e0f9cb0c7d192224854b
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-09 12:46:43 +0100

    featuredPosts reducer combined in rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
 import loginReducer from './reducers/login.reducer';
 import userReducer from './reducers/user.reducer';
+import featuredReducer from './reducers/featured.reducer';
 
-const rootReducer = combineReducers({ login: loginReducer, user: userReducer });
+const rootReducer = combineReducers({
+  login: loginReducer,
+  user: userReducer,
+  featuredPosts: featuredReducer
+});
 
 export default rootReducer;
