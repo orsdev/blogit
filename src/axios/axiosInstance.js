eec92289b18e06070b905971bd0d338d36bfc2fe
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-09 12:45:54 +0100

    env variable modified in axiosInstance

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BLOGIT_DB_URL,
+  baseURL: process.env.REACT_APP_BLOGITDB_URL,
   timeout: 10000, //wait for 10 seconds
   headers: {
     'Content-Type': 'Application/json'
