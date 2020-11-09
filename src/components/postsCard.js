71cedcc28b6d566e5c7b70ac4c46788cc315e310
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-09 18:29:33 +0100

    postsCard posts added dynamically

--- src/components/postsCard.js
+++ src/components/postsCard.js
@@ -1,167 +1,133 @@
-import React from 'react';
-import Ilya from '../assets/images/ilya.jpg';
-import Moped from '../assets/images/moped.jpg';
-import Justin from '../assets/images/justin.jpg';
-import Bruno from '../assets/images/bruno.jpg';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Alert, Skeleton } from 'antd';
+import { getPosts } from '../redux/actions/postsCard.action';
 
-function postsCard() {
+function PostsCard({ posts, error, onGetPosts }) {
+  useEffect(() => {
+    onGetPosts();
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    <section className="blogCards py-3">
-      <h5 className="blogCards-title p-2">Must Read</h5>
-      <article className="blogCards-article">
-        <img src={Ilya} alt="Article Cover" />
-        <p className="blogCards-article-text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo,
-          ducimus...
-        </p>
-        <span className="blogCards-article-author">Timberly -</span>
-        <span className="blogCards-article-date">
-          {new Date().toDateString()}
-        </span>
-        <span className="blogCards-article-view mx-4">
-          <i className="fa fa-eye pr-2" aria-hidden="true"></i>
-          <b>10</b>
-        </span>
-        <span className="blogCards-article-comment">
-          <i className="fa fa-comment-o pr-2" aria-hidden="true"></i>
-          <b>120</b>
-        </span>
-      </article>
-      <article className="blogCards-article">
-        <img src={Moped} alt="Article Cover" />
-        <p className="blogCards-article-text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo,
-          ducimus...
-        </p>
-        <span className="blogCards-article-author">Timberly -</span>
-        <span className="blogCards-article-date">
-          {new Date().toDateString()}
-        </span>
-        <span className="blogCards-article-view mx-4">
-          <i className="fa fa-eye pr-2" aria-hidden="true"></i>
-          <b>10</b>
-        </span>
-        <span className="blogCards-article-comment">
-          <i className="fa fa-comment-o pr-2" aria-hidden="true"></i>
-          <b>120</b>
-        </span>
-      </article>
-      <article className="blogCards-article">
-        <img src={Justin} alt="Article Cover" />
-        <p className="blogCards-article-text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo,
-          ducimus...
-        </p>
-        <span className="blogCards-article-author">Timberly -</span>
-        <span className="blogCards-article-date">
-          {new Date().toDateString()}
-        </span>
-        <span className="blogCards-article-view mx-4">
-          <i className="fa fa-eye pr-2" aria-hidden="true"></i>
-          <b>10</b>
-        </span>
-        <span className="blogCards-article-comment">
-          <i className="fa fa-comment-o pr-2" aria-hidden="true"></i>
-          <b>120</b>
-        </span>
-      </article>
-      <article className="blogCards-article">
-        <img src={Bruno} alt="Article Cover" />
-        <p className="blogCards-article-text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo,
-          ducimus...
-        </p>
-        <span className="blogCards-article-author">Timberly -</span>
-        <span className="blogCards-article-date">
-          {new Date().toDateString()}
-        </span>
-        <span className="blogCards-article-view mx-4">
-          <i className="fa fa-eye pr-2" aria-hidden="true"></i>
-          <b>10</b>
-        </span>
-        <span className="blogCards-article-comment">
-          <i className="fa fa-comment-o pr-2" aria-hidden="true"></i>
-          <b>120</b>
-        </span>
-      </article>
-      <article className="blogCards-article">
-        <img src={Ilya} alt="Article Cover" />
-        <p className="blogCards-article-text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo,
-          ducimus...
-        </p>
-        <span className="blogCards-article-author">Timberly -</span>
-        <span className="blogCards-article-date">
-          {new Date().toDateString()}
-        </span>
-        <span className="blogCards-article-view mx-4">
-          <i className="fa fa-eye pr-2" aria-hidden="true"></i>
-          <b>10</b>
-        </span>
-        <span className="blogCards-article-comment">
-          <i className="fa fa-comment-o pr-2" aria-hidden="true"></i>
-          <b>120</b>
-        </span>
-      </article>
-      <article className="blogCards-article">
-        <img src={Moped} alt="Article Cover" />
-        <p className="blogCards-article-text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo,
-          ducimus...
-        </p>
-        <span className="blogCards-article-author">Timberly -</span>
-        <span className="blogCards-article-date">
-          {new Date().toDateString()}
-        </span>
-        <span className="blogCards-article-view mx-4">
-          <i className="fa fa-eye pr-2" aria-hidden="true"></i>
-          <b>10</b>
-        </span>
-        <span className="blogCards-article-comment">
-          <i className="fa fa-comment-o pr-2" aria-hidden="true"></i>
-          <b>120</b>
-        </span>
-      </article>
-      <article className="blogCards-article">
-        <img src={Justin} alt="Article Cover" />
-        <p className="blogCards-article-text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo,
-          ducimus...
-        </p>
-        <span className="blogCards-article-author">Timberly -</span>
-        <span className="blogCards-article-date">
-          {new Date().toDateString()}
-        </span>
-        <span className="blogCards-article-view mx-4">
-          <i className="fa fa-eye pr-2" aria-hidden="true"></i>
-          <b>10</b>
-        </span>
-        <span className="blogCards-article-comment">
-          <i className="fa fa-comment-o pr-2" aria-hidden="true"></i>
-          <b>120</b>
-        </span>
-      </article>
-      <article className="blogCards-article">
-        <img src={Bruno} alt="Article Cover" />
-        <p className="blogCards-article-text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo,
-          ducimus...
-        </p>
-        <span className="blogCards-article-author">Timberly -</span>
-        <span className="blogCards-article-date">
-          {new Date().toDateString()}
-        </span>
-        <span className="blogCards-article-view mx-4">
-          <i className="fa fa-eye pr-2" aria-hidden="true"></i>
-          <b>10</b>
-        </span>
-        <span className="blogCards-article-comment">
-          <i className="fa fa-comment-o pr-2" aria-hidden="true"></i>
-          <b>120</b>
-        </span>
-      </article>
-    </section>
+    <>
+      {error ? (
+        <div className="mx-auto mt-5">
+          <Alert message={error} type="error" showIcon closable />
+        </div>
+      ) : null}
+      <section className="blogCards py-3">
+        {!error && posts && posts.length ? (
+          <>
+            <h5 className="blogCards-title p-2">Must Read</h5>
+            {posts.map((post) => {
+              return (
+                <article key={post._id} className="blogCards-article">
+                  <img src={post.coverImage} alt="Article Cover" />
+                  <p className="blogCards-article-text mb-1">{post.body}</p>
+                  <span className="blogCards-article-author">
+                    {post.user.username} -
+                  </span>
+                  <span className="blogCards-article-date">
+                    {new Date(post.createdAt).toDateString()}
+                  </span>
+                  <span className="blogCards-article-comment ml-3">
+                    <b>
+                      <i
+                        className="fa fa-comment-o pr-1"
+                        aria-hidden="true"></i>
+                    </b>
+                    <b>{post.comments.length}</b>
+                  </span>
+                </article>
+              );
+            })}
+          </>
+        ) : (
+          <>
+            <Skeleton active={true} loading={true}>
+              <article className="blogCards-article">
+                <p className="blogCards-article-text">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Nemo, ducimus...
+                </p>
+                <span className="blogCards-article-author">Timberly -</span>
+                <span className="blogCards-article-date">
+                  {new Date().toDateString()}
+                </span>
+                <span className="blogCards-article-view mx-4">
+                  <i className="fa fa-eye pr-2" aria-hidden="true"></i>
+                  <b>10</b>
+                </span>
+                <span className="blogCards-article-comment">
+                  <i className="fa fa-comment-o pr-2" aria-hidden="true"></i>
+                  <b>120</b>
+                </span>
+              </article>
+            </Skeleton>
+            <Skeleton active={true} loading={true}>
+              <article className="blogCards-article">
+                <p className="blogCards-article-text">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Nemo, ducimus...
+                </p>
+                <span className="blogCards-article-author">Timberly -</span>
+                <span className="blogCards-article-date">
+                  {new Date().toDateString()}
+                </span>
+                <span className="blogCards-article-view mx-4">
+                  <i className="fa fa-eye pr-2" aria-hidden="true"></i>
+                  <b>10</b>
+                </span>
+                <span className="blogCards-article-comment">
+                  <i className="fa fa-comment-o pr-2" aria-hidden="true"></i>
+                  <b>120</b>
+                </span>
+              </article>
+            </Skeleton>
+            <Skeleton active={true} loading={true}>
+              <article className="blogCards-article">
+                <p className="blogCards-article-text">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Nemo, ducimus...
+                </p>
+                <span className="blogCards-article-author">Timberly -</span>
+                <span className="blogCards-article-date">
+                  {new Date().toDateString()}
+                </span>
+                <span className="blogCards-article-view mx-4">
+                  <i className="fa fa-eye pr-2" aria-hidden="true"></i>
+                  <b>10</b>
+                </span>
+                <span className="blogCards-article-comment">
+                  <i className="fa fa-comment-o pr-2" aria-hidden="true"></i>
+                  <b>120</b>
+                </span>
+              </article>
+            </Skeleton>
+          </>
+        )}
+      </section>
+    </>
   );
 }
 
-export default postsCard;
+PostsCard.propTypes = {
+  posts: PropTypes.any,
+  error: PropTypes.any,
+  onGetPosts: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  posts: state.postsCard.posts,
+  error: state.postsCard.error
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onGetPosts: () => dispatch(getPosts())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsCard);
