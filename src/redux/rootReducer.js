ff6530d6b3cce715a3baeb1cc133c7ec8be7233d
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-09 18:31:17 +0100

    postsCard reducer combine in rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import loginReducer from './reducers/login.reducer';
 import userReducer from './reducers/user.reducer';
 import featuredReducer from './reducers/featured.reducer';
+import postsCardReducer from './reducers/postsCard.reducer';
 
 const rootReducer = combineReducers({
   login: loginReducer,
   user: userReducer,
-  featuredPosts: featuredReducer
+  featuredPosts: featuredReducer,
+  postsCard: postsCardReducer
 });
 
 export default rootReducer;
