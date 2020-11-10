7e33f8cf2ee27eeb8a912a682422c94dda767ab8
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-10 19:39:04 +0100

    paginationLengthReducer combined in rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,12 +3,14 @@ import loginReducer from './reducers/login.reducer';
 import userReducer from './reducers/user.reducer';
 import featuredReducer from './reducers/featured.reducer';
 import postsCardReducer from './reducers/postsCard.reducer';
+import paginationLengthReducer from './reducers/paginationLength.reducer';
 
 const rootReducer = combineReducers({
   login: loginReducer,
   user: userReducer,
   featuredPosts: featuredReducer,
-  postsCard: postsCardReducer
+  postsCard: postsCardReducer,
+  paginationLength: paginationLengthReducer
 });
 
 export default rootReducer;
