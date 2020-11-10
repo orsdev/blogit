bf38ad6597601538f6b2f689e7317e09f211c473
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-10 19:42:15 +0100

    loading state added to postsCard component

--- src/components/postsCard.js
+++ src/components/postsCard.js
@@ -4,9 +4,10 @@ import PropTypes from 'prop-types';
 import { Alert, Skeleton } from 'antd';
 import { getPosts } from '../redux/actions/postsCard.action';
 
-function PostsCard({ posts, error, onGetPosts }) {
+function PostsCard({ loading, posts, error, onGetPosts }) {
   useEffect(() => {
-    onGetPosts();
+    // call dispatch function
+    onGetPosts('/posts');
     // eslint-disable-next-line
   }, []);
 
@@ -25,7 +26,9 @@ function PostsCard({ posts, error, onGetPosts }) {
               return (
                 <article key={post._id} className="blogCards-article">
                   <img src={post.coverImage} alt="Article Cover" />
-                  <p className="blogCards-article-text mb-1">{post.body}</p>
+                  <p className="blogCards-article-text mb-1">
+                    {`${post.body.substring(0, 100)}...`}
+                  </p>
                   <span className="blogCards-article-author">
                     {post.user.username} -
                   </span>
@@ -44,24 +47,31 @@ function PostsCard({ posts, error, onGetPosts }) {
               );
             })}
           </>
-        ) : (
+        ) : null}
+        {loading ? (
           <>
             <Skeleton active={true} loading={true}>
               <article className="blogCards-article">
                 <p className="blogCards-article-text">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Nemo, ducimus...
+                  Lorem ipsum dolor sit, amet consectetur adipisicing
+                  elit. Nemo, ducimus...
                 </p>
-                <span className="blogCards-article-author">Timberly -</span>
+                <span className="blogCards-article-author">
+                  Timberly -
+                </span>
                 <span className="blogCards-article-date">
                   {new Date().toDateString()}
                 </span>
                 <span className="blogCards-article-view mx-4">
-                  <i className="fa fa-eye pr-2" aria-hidden="true"></i>
+                  <i
+                    className="fa fa-eye pr-2"
+                    aria-hidden="true"></i>
                   <b>10</b>
                 </span>
                 <span className="blogCards-article-comment">
-                  <i className="fa fa-comment-o pr-2" aria-hidden="true"></i>
+                  <i
+                    className="fa fa-comment-o pr-2"
+                    aria-hidden="true"></i>
                   <b>120</b>
                 </span>
               </article>
@@ -69,19 +79,25 @@ function PostsCard({ posts, error, onGetPosts }) {
             <Skeleton active={true} loading={true}>
               <article className="blogCards-article">
                 <p className="blogCards-article-text">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Nemo, ducimus...
+                  Lorem ipsum dolor sit, amet consectetur adipisicing
+                  elit. Nemo, ducimus...
                 </p>
-                <span className="blogCards-article-author">Timberly -</span>
+                <span className="blogCards-article-author">
+                  Timberly -
+                </span>
                 <span className="blogCards-article-date">
                   {new Date().toDateString()}
                 </span>
                 <span className="blogCards-article-view mx-4">
-                  <i className="fa fa-eye pr-2" aria-hidden="true"></i>
+                  <i
+                    className="fa fa-eye pr-2"
+                    aria-hidden="true"></i>
                   <b>10</b>
                 </span>
                 <span className="blogCards-article-comment">
-                  <i className="fa fa-comment-o pr-2" aria-hidden="true"></i>
+                  <i
+                    className="fa fa-comment-o pr-2"
+                    aria-hidden="true"></i>
                   <b>120</b>
                 </span>
               </article>
@@ -89,25 +105,31 @@ function PostsCard({ posts, error, onGetPosts }) {
             <Skeleton active={true} loading={true}>
               <article className="blogCards-article">
                 <p className="blogCards-article-text">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Nemo, ducimus...
+                  Lorem ipsum dolor sit, amet consectetur adipisicing
+                  elit. Nemo, ducimus...
                 </p>
-                <span className="blogCards-article-author">Timberly -</span>
+                <span className="blogCards-article-author">
+                  Timberly -
+                </span>
                 <span className="blogCards-article-date">
                   {new Date().toDateString()}
                 </span>
                 <span className="blogCards-article-view mx-4">
-                  <i className="fa fa-eye pr-2" aria-hidden="true"></i>
+                  <i
+                    className="fa fa-eye pr-2"
+                    aria-hidden="true"></i>
                   <b>10</b>
                 </span>
                 <span className="blogCards-article-comment">
-                  <i className="fa fa-comment-o pr-2" aria-hidden="true"></i>
+                  <i
+                    className="fa fa-comment-o pr-2"
+                    aria-hidden="true"></i>
                   <b>120</b>
                 </span>
               </article>
             </Skeleton>
           </>
-        )}
+        ) : null}
       </section>
     </>
   );
@@ -116,18 +138,23 @@ function PostsCard({ posts, error, onGetPosts }) {
 PostsCard.propTypes = {
   posts: PropTypes.any,
   error: PropTypes.any,
+  loading: PropTypes.bool,
   onGetPosts: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state) => ({
   posts: state.postsCard.posts,
-  error: state.postsCard.error
+  error: state.postsCard.error,
+  loading: state.postsCard.loading
 });
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onGetPosts: () => dispatch(getPosts())
+    onGetPosts: (url) => dispatch(getPosts(url))
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(PostsCard);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PostsCard);
