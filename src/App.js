a8fe46e9ad1d0d808a2f909d5bec559a6fea5705
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-11 18:41:40 +0100

    error handling added to App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import Router from './router/router';
 import './assets/css/style.css';
 import 'antd/dist/antd.css';
 
-function App() {
+function App({ error, history }) {
+  useEffect(() => {
+    if (error) {
+      history.replace('/500');
+    }
+    // eslint-disable-next-line
+  }, [error]);
+
   return (
     <>
       <Router />
@@ -10,4 +21,13 @@ function App() {
   );
 }
 
-export default App;
+App.propTypes = {
+  error: PropTypes.any,
+  history: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  error: state.error.globalError
+});
+
+export default connect(mapStateToProps)(withRouter(App));
