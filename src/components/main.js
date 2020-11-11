c0d4cbf2f3e53034dcff5b52aae55381ae29f703
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-11 08:24:24 +0100

    error 500 page redirection added to Main component

--- src/components/main.js
+++ src/components/main.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import PostsCard from './postsCard';
 import Featured from './featured';
 import Pagination from './pagination';
 
-function Main() {
+function Main({ error, history }) {
+  useEffect(() => {
+    if (error) {
+      history.replace('/500');
+    }
+    // eslint-disable-next-line
+  }, [error]);
+
   return (
-    <main className="main mb-5">
-      <Featured />
-      <PostsCard />
-      <Pagination />
-    </main>
+    <>
+      <main className="main mb-5">
+        <Featured />
+        <PostsCard />
+        <Pagination />
+      </main>
+    </>
   );
 }
 
-export default Main;
+Main.propTypes = {
+  error: PropTypes.any,
+  history: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  error: state.error.globalError
+});
+
+export default connect(mapStateToProps)(withRouter(Main));
