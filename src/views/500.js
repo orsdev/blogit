dd9892b92f67e081fbf5162fc6ddd493721332a9
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-11 08:03:28 +0100

    500 error component added to app

---
+++ src/views/500.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Result } from 'antd';
+import PropTypes from 'prop-types';
+
+function Page500({ error, history }) {
+  useEffect(() => {
+    if (!error) {
+      history.replace('/');
+    }
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <section className="page500">
+      <h5 className="page500-logo">Blogit</h5>
+      <Result
+        status="500"
+        title="500"
+        subTitle="Something went wrong. Please refresh your browser."
+      />
+    </section>
+  );
+}
+
+Page500.propTypes = {
+  error: PropTypes.any,
+  history: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  error: state.error.globalError
+});
+
+export default connect(mapStateToProps)(Page500);
