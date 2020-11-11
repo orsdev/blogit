3a5f9e5de0fd26b883c440b2b947f8f3966fdbed
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-11 18:41:05 +0100

    error handling removed from Main component

--- src/components/main.js
+++ src/components/main.js
@@ -1,19 +1,9 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React from 'react';
 import PostsCard from './postsCard';
 import Featured from './featured';
 import Pagination from './pagination';
 
-function Main({ error, history }) {
-  useEffect(() => {
-    if (error) {
-      history.replace('/500');
-    }
-    // eslint-disable-next-line
-  }, [error]);
-
+function Main() {
   return (
     <>
       <main className="main mb-5">
@@ -25,13 +15,4 @@ function Main({ error, history }) {
   );
 }
 
-Main.propTypes = {
-  error: PropTypes.any,
-  history: PropTypes.object.isRequired
-};
-
-const mapStateToProps = (state) => ({
-  error: state.error.globalError
-});
-
-export default connect(mapStateToProps)(withRouter(Main));
+export default Main;
