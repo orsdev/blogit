487a7a4837a03660e87b4f98fc7939259a295709
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-11 13:45:59 +0100

    signup component formatted and form minor bugs fixed

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,22 +1,71 @@
-import React, { useState } from 'react';
-import { Form, Input, Button } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Form, Input, Button, Alert } from 'antd';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { signup } from '../redux/actions/signup.action';
 
-const Signup = () => {
+const Signup = ({ error, token, onSignup }) => {
   const [disableButton, setDisableButton] = useState(false);
+  const [buttonLoading, setButtonLoading] = useState(false);
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    if (token) {
+      // update states
+      setDisableButton(false);
+      setButtonLoading(false);
+
+      // clear form fields
+      form.resetFields();
+    }
+    // eslint-disable-next-line
+  }, [token]);
+
+  useEffect(() => {
+    // update states
+    setDisableButton(false);
+    setButtonLoading(false);
+  }, [error]);
 
   const onFinish = (values) => {
+    // update states
     setDisableButton(true);
+    setButtonLoading(true);
+
+    // call dispatch function
+    onSignup(values);
   };
 
   return (
     <>
       <section className="signup">
-        <Link exact to="/" className="signup-logo">
+        <Link to="/" className="signup-logo">
           Blogit
         </Link>
-        <div className="signup-body">
-          <Form name="signup" onFinish={onFinish}>
+        <div className="signup-body mb-5">
+          <Form form={form} name="signup" onFinish={onFinish}>
+            {token ? (
+              <div className="signup-body-success mx-auto mb-4">
+                <Alert
+                  message={
+                    <>
+                      <span>Account created successfully.</span>
+                      <Link to="/login" className="ml-2 signup-link">
+                        Log in
+                      </Link>
+                    </>
+                  }
+                  type="success"
+                  showIcon
+                />
+              </div>
+            ) : null}
+            {error && typeof error === 'string' ? (
+              <div className="signup-body-error mx-auto mb-4">
+                <Alert message={error} type="error" showIcon />
+              </div>
+            ) : null}
             <h6>Create your Account!</h6>
             <Form.Item
               name="username"
@@ -24,8 +73,8 @@ const Signup = () => {
               rules={[
                 {
                   required: true,
-                  min: 4,
-                },
+                  min: 4
+                }
               ]}>
               <Input />
             </Form.Item>
@@ -35,8 +84,8 @@ const Signup = () => {
               rules={[
                 {
                   type: 'email',
-                  required: true,
-                },
+                  required: true
+                }
               ]}>
               <Input />
             </Form.Item>
@@ -46,47 +95,72 @@ const Signup = () => {
               rules={[
                 {
                   required: true,
-                  message: 'Please input your password!',
-                  min: 6,
-                },
+                  message: 'Password must atleast be 6 characters!',
+                  min: 6
+                }
               ]}>
               <Input type="password" />
             </Form.Item>
             <Form.Item
-              name="confirm"
+              name="confirm_password"
               label="Confirm Password"
               dependencies={['password']}
               rules={[
                 {
                   required: true,
-                  message: 'Please confirm your password!',
-                  min: 6,
+                  message: 'Please confirm your password!'
                 },
                 ({ getFieldValue }) => ({
                   validator(rule, value) {
-                    if (!value || getFieldValue('password') === value) {
+                    if (
+                      !value ||
+                      getFieldValue('password') === value
+                    ) {
                       return Promise.resolve();
                     }
 
                     return Promise.reject(
                       'The two passwords that you entered do not match!'
                     );
-                  },
-                }),
+                  }
+                })
               ]}>
               <Input type="password" />
             </Form.Item>
             <Form.Item className="m-0 p-0">
-              <Button disabled={disableButton} type="primary" htmlType="submit">
+              <Button
+                disabled={disableButton}
+                loading={buttonLoading}
+                type="primary"
+                htmlType="submit">
                 Submit
               </Button>
             </Form.Item>
             <Form.Item className="m-0 p-0">
               <span>Already have an account?</span>
-              <Link exact to="/login" className="ml-2 signup-link">
+              <Link to="/login" className="ml-2 signup-link">
                 Log in
               </Link>
             </Form.Item>
+            {error && Array.isArray(error) ? (
+              <div className="signup-body-error mx-auto mt-4">
+                <Alert
+                  message={error.map((value, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className="h4 list-item text-danger py-1 d-flex">
+                        <span className="font-weight-bold text-danger">
+                          &#42;
+                        </span>
+                        {value}
+                      </li>
+                    );
+                  })}
+                  type="error"
+                />
+              </div>
+            ) : null}
           </Form>
         </div>
       </section>
@@ -94,4 +168,21 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+Signup.propTypes = {
+  token: PropTypes.string,
+  error: PropTypes.any,
+  onSignup: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  token: state.signup.token,
+  error: state.signup.error
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSignup: (userData) => dispatch(signup(userData))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Signup);
