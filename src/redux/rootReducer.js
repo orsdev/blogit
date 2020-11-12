c0b8105e3e88aeee2c9cc27beba3ef9593e81ceb
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-12 04:35:47 +0100

    tokenReducer combined in root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import loginReducer from './reducers/login.reducer';
 import signupReducer from './reducers/signup.reducer';
 import userReducer from './reducers/user.reducer';
+import tokenReducer from './reducers/token.reducer';
 import featuredReducer from './reducers/featured.reducer';
 import postsCardReducer from './reducers/postsCard.reducer';
 import singlePostReducer from './reducers/singlePost.reducer';
@@ -11,6 +12,7 @@ import errorReducer from './reducers/error.reducer';
 const rootReducer = combineReducers({
   login: loginReducer,
   signup: signupReducer,
+  token: tokenReducer,
   user: userReducer,
   featuredPosts: featuredReducer,
   postsCard: postsCardReducer,
