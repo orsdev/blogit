661c01f0ad2cbaa4c4cb9396dcea1d1479990b99
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-12 14:05:11 +0100

    automatic cookie tokken sign in removed from home component

--- src/views/home.js
+++ src/views/home.js
@@ -1,33 +1,8 @@
-import React, { useEffect } from 'react';
-import cookie from 'react-cookies';
-import { connect } from 'react-redux';
-import jwt_decode from 'jwt-decode';
-import PropTypes from 'prop-types';
-import { setUser } from '../redux/actions/user.action';
-import { setToken } from '../redux/actions/token.action';
+import React from 'react';
 import Header from '../components/header';
 import Main from '../components/main';
 
-function Home({ token, onSetUser, onSetToken }) {
-  useEffect(() => {
-    if (!token) {
-      const getCookieFromStorage = cookie.load('token');
-
-      if (getCookieFromStorage) {
-        // call dispatch function
-        onSetToken(getCookieFromStorage);
-
-        const decoded = jwt_decode(getCookieFromStorage);
-
-        if (decoded) {
-          // call dispatch function
-          onSetUser(decoded);
-        }
-      }
-    }
-    // eslint-disable-next-line
-  }, []);
-
+function Home() {
   return (
     <>
       <Header />
@@ -36,21 +11,4 @@ function Home({ token, onSetUser, onSetToken }) {
   );
 }
 
-Home.propTypes = {
-  token: PropTypes.any,
-  onSetUser: PropTypes.func.isRequired,
-  onSetToken: PropTypes.func.isRequired
-};
-
-const mapStateToProps = (state) => ({
-  token: state.token
-});
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onSetUser: (user) => dispatch(setUser(user)),
-    onSetToken: (token) => dispatch(setToken(token))
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
