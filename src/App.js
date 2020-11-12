2fec1715a499015e71497710fd3d6a4881728609
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-12 14:08:29 +0100

    error handling and tokken login feature added to App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import cookie from 'react-cookies';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import Router from './router/router';
+import jwt_decode from 'jwt-decode';
+import { setUser } from './redux/actions/user.action';
+import { setToken } from './redux/actions/token.action';
 import './assets/css/style.css';
 import 'antd/dist/antd.css';
 
-function App({ error, history }) {
+function App({ error, history, token, onSetUser, onSetToken }) {
   useEffect(() => {
     if (error) {
       history.replace('/500');
     }
     // eslint-disable-next-line
   }, [error]);
+
+  useEffect(() => {
+    if (!token) {
+      const getCookieFromStorage = cookie.load('token');
+
+      if (getCookieFromStorage) {
+        // call dispatch function
+        onSetToken(getCookieFromStorage);
+
+        const decoded = jwt_decode(getCookieFromStorage);
+
+        if (decoded) {
+          // call dispatch function
+          onSetUser(decoded);
+        }
+      }
+    }
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <>
@@ -22,12 +45,26 @@ function App({ error, history }) {
 }
 
 App.propTypes = {
+  token: PropTypes.string,
+  onSetUser: PropTypes.func.isRequired,
+  onSetToken: PropTypes.func.isRequired,
   error: PropTypes.any,
   history: PropTypes.object.isRequired
 };
 
 const mapStateToProps = (state) => ({
-  error: state.error.globalError
+  error: state.error.globalError,
+  token: state.token
 });
 
-export default connect(mapStateToProps)(withRouter(App));
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSetUser: (user) => dispatch(setUser(user)),
+    onSetToken: (token) => dispatch(setToken(token))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(App));
