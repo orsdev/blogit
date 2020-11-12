1394d368273340c8e99e7f1abe396ad0123b775f
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-12 04:40:27 +0100

    automatic signin added to home component

--- src/views/home.js
+++ src/views/home.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import cookie from 'react-cookies';
+import { connect } from 'react-redux';
+import jwt_decode from 'jwt-decode';
+import PropTypes from 'prop-types';
+import { setUser } from '../redux/actions/user.action';
+import { setToken } from '../redux/actions/token.action';
 import Header from '../components/header';
 import Main from '../components/main';
 
-function Home() {
+function Home({ token, onSetUser, onSetToken }) {
+  useEffect(() => {
+    if (!token) {
+      const getCookieFromStorage = cookie.load('token');
+
+      if (getCookieFromStorage) {
+        // call dispatch function
+        onSetToken(getCookieFromStorage);
+
+        const decoded = jwt_decode(getCookieFromStorage);
+
+        if (decoded) {
+          // call dispatch function
+          onSetUser(decoded);
+        }
+      }
+    }
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <>
       <Header />
@@ -11,4 +36,21 @@ function Home() {
   );
 }
 
-export default Home;
+Home.propTypes = {
+  token: PropTypes.any,
+  onSetUser: PropTypes.func.isRequired,
+  onSetToken: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  token: state.token
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSetUser: (user) => dispatch(setUser(user)),
+    onSetToken: (token) => dispatch(setToken(token))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
